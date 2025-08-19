130fd645ab3139d3a244ea4d6ea557c58247c55c
Bruno Bispo <brunobispoo25@gmail.com>
2025-08-19 20:20:57 -0300

    Testes unitarios

---
+++ preparacao_projeto/tests/calculator_1_test.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.append('../')
+
+from src.calculators.calculaor_1 import Calculator1
+from typing import Dict
+from pytest import raises
+
+class MockRequest:
+  def __init__(self, body: Dict) -> None:
+    self.json = body
+
+def test_calculate():
+  mock_request = MockRequest(body={'number': 1})
+
+  calculator1 = Calculator1()
+
+  response = calculator1.calculate(mock_request)
+  
+  # Formato da resposta
+  assert "data" in response
+  assert "calculator" in response['data']
+  assert "result" in response['data']
+
+  # Assertividade da resposta
+  assert response['data']['result'] == 14.25
+  assert response['data']['calculator'] == 1
+
+def test_calculate_with_body_error():
+  mock_request = MockRequest(body={'something': 1})
+
+  calculator1 = Calculator1()
+
+  with raises(Exception) as excinfo:
+    calculator1.calculate(mock_request)
+
+  assert str(excinfo.value) == "body mal formatado!"
+  
+  
